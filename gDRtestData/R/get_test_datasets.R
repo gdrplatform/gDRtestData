d8bf742b60081447b20a608f1806c1223c899884
Kamil Foltynski <kamil.foltynski@contractors.roche.com>
2021-10-12 10:20:43 +0000

    PR remarks applied

--- gDRtestData/R/get_test_datasets.R
+++ gDRtestData/R/get_test_datasets.R
@@ -14,15 +14,27 @@ get_config_path <- function() {
 #' 
 #' Returns named vector of absolute paths to test datasets.
 #'
+#' @param datasets_dir path to directory with datasets (default \code{NULL}).
+#' If \code{NULL}, then \code{inst/testdata} directory from \code{gDRtestData} will be used.
+#' @param config_path path to config i \code{yml} format (default \code{NULL}).
+#' If \code{NULL}, then config from \link{get_config_path} will be used.
+#'
 #' @return named vector of absolute paths
 #' @export
 #'
 #' @author Kamil Foltyński \email{kamil.foltynski@@contractors.roche.com}
 #'
 #' @rdname get_test_dataset_paths
-get_test_dataset_paths <- function() {
-  datasets_dir <- system.file("testdata", package = "gDRtestData", mustWork = TRUE)
-  config <- yaml::read_yaml(get_config_path())
+get_test_dataset_paths <- function(datasets_dir = NULL, config_path = NULL) {
+  if (is.null(datasets_dir))
+    datasets_dir <- system.file("testdata", package = "gDRtestData", mustWork = TRUE)
+  checkmate::assert_string(datasets_dir, min.chars = 1)
+  checkmate::assert_directory_exists(datasets_dir)
+  if (is.null(config_path))
+    config_path <- get_config_path()
+  checkmate::assert_string(config_path, min.chars = 1)
+  checkmate::assert_file_exists(config_path)
+  config <- yaml::read_yaml(config_path)
   out <- vapply(config, function(x) {
     checkmate::assert_string(x$file, min.chars = 1)
     fpath <- file.path(datasets_dir, x$file)
--- gDRtestData/tests/testthat/test_get_test_dataset_paths.R
+++ gDRtestData/tests/testthat/test_get_test_dataset_paths.R
@@ -8,4 +8,18 @@ testthat::test_that("config exists", {
 testthat::test_that("test dataset paths exist", {
   testthat::expect_type(get_test_dataset_paths(), "character")
   testthat::expect_gt(length(get_test_dataset_paths()), 0L)
+  dir.create(tmp_dir <- tempfile())
+  on.exit(unlink(tmp_dir, TRUE, TRUE))
+  testthat::expect_error(get_test_dataset_paths(datasets_dir = ""),
+                         "Assertion on 'datasets_dir' failed: Must have at least 1 characters.")
+  testthat::expect_error(get_test_dataset_paths(datasets_dir = character(0)),
+                         "Assertion on 'datasets_dir' failed: Must have length 1.")
+  testthat::expect_error(get_test_dataset_paths(datasets_dir = tmp_dir),
+                         "Assertion on 'fpath' failed: File does not exist")
+  testthat::expect_error(get_test_dataset_paths(config_path = tempfile()),
+                         "Assertion on 'config_path' failed: File does not exist")
+  testthat::expect_error(get_test_dataset_paths(config_path = ""),
+                         "Assertion on 'config_path' failed: Must have at least 1 characters.")
+  testthat::expect_error(get_test_dataset_paths(config_path = character(0)),
+                         "Assertion on 'config_path' failed: Must have length 1.")
 })
