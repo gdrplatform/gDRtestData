a0c23c64e4f96010248db0853bd8d41beab12f80
Marcin Kamianowski <marcin.kamianowski@contractors.roche.com>
2022-09-05 22:27:36 +0200

    fix: move script

--- rplatform/run_tests.sh
+++ rplatform/run_tests.sh
@@ -9,15 +9,5 @@ echo "Working directory contains: `ls | tr '\n' ' '`"
 # exit when any command fails
 set -e
 
-echo ">>>>>>>> Running linter"
-Rscript -e "gDRstyle::lintPkgDirs('/mnt/vol/gDRtestData')"
-
-echo ">>>>> RUNNING UNIT TESTS"
-Rscript -e "testthat::test_local(path = '$repo_path', stop_on_failure = TRUE)"
-
-echo ">>>>> RUNNING CHECK"
-R CMD build /mnt/vol/gDRtestData &&
-    R CMD check gDRtestData_*.tar.gz --no-vignettes --no-examples --no-manual --no-tests
-
-echo ">>>>>>>> RUNNING CHECK DEPENDENCIES"
-Rscript -e "gDRstyle::checkDependencies(desc_path='/mnt/vol/gDRtestData/DESCRIPTION', dep_path='/mnt/vol/rplatform/dependencies.yaml')"
+echo ">>>>>>>> RUNNING CHECK"
+Rscript -e "gDRstyle::checkPackage('gDRutils' , '$repo_path', TRUE)"
