a07f711be1f3f2a3324870198ad0547a863b1af8
czechb3 <bartosz.czech@contractors.roche.com>
2023-03-27 18:51:05 +0200

    doc: appease BiocCheck requirements

---
+++ R/generate_data.R
@@ -0,0 +1,152 @@
+#' Add data replicates
+#'
+#' @param df_layout data.frame that should contains the cell line,
+#' drug, concentration, and replicate columns along with the annotations that needs to be propagated
+#'
+#'
+#'
+#' @examples
+#' 
+#' cell_lines <- create_synthetic_cell_lines()
+#' add_data_replicates(cell_lines)
+#' 
+#' @return data.frame with replicates
+#' @export
+#'
+add_data_replicates <- function(df_layout) {
+  df_layout_duplicates <- Reduce(rbind, list(df_layout)[rep(1, times = 3)])
+  barcode <- do.call(c, lapply(paste0("plate_", seq_len(3)), function(x) rep(x, nrow(df_layout))))
+  cbind(Barcode = barcode, df_layout_duplicates)
+}
+
+
+#' Add concentrations
+#'
+#' @param df_layout data.frame that should contains the cell line,
+#' drug, concentration, and replicate columns along with the annotations that needs to be propagated
+#' @param concentrations vector of numeric concentrations that will be added to df_layout
+#'
+#' @examples
+#' 
+#' cell_lines <- create_synthetic_cell_lines()
+#' add_concentration(cell_lines)
+#' 
+#' @return data.frame with concentrations
+#' @export
+#'
+add_concentration <- function(df_layout, concentrations = 10 ^ (seq(-3, 1, 0.5))) {
+  df_layout <- merge(df_layout, data.frame(Concentration = c(0, 0, concentrations)), by = NULL)
+  df_layout
+}
+
+
+#' Generate response data
+#'
+#' @param df_layout data.frame that should contains the cell line,
+#' drug, concentration, and replicate columns along with the annotations that needs to be propagated
+#' @param noise_level numeric scalar with the level of noise added to the data
+#'
+#'
+#' @examples
+#' 
+#' cell_lines <- create_synthetic_cell_lines()
+#' drugs <- create_synthetic_drugs()
+#' prepareData(cell_lines[seq_len(2), ], drugs[seq_len(4), ])
+#' 
+#'
+#' @return data.frame with response data
+#' @export
+#'
+generate_response_data <- function(df_layout, noise_level = 0.1) {
+  drugs <- create_synthetic_drugs()
+  cell_lines <- create_synthetic_cell_lines()
+  hill_coef <- generate_hill_coef(drugs, cell_lines)
+  ec50 <- generate_ec50(drugs, cell_lines)
+  e_inf <- generate_e_inf(drugs, cell_lines)
+
+  df_layout$ReadoutValue <- round(100 * pmax(
+    getReadoutCoef(df_layout, e_inf, ec50, hill_coef) +
+      (noise_level * stats::runif(nrow(df_layout)) - (noise_level / 2)),  # add some noise
+    0.01 * stats::runif(nrow(df_layout)) + 0.005), # avoid hard 0 values
+    1)
+  df_layout$BackgroundValue <- 0
+  df_layout$Duration <- 72
+  df_layout <- introduceVehicle(df_layout)
+
+  if ("Gnumber_2" %in% colnames(df_layout)) { # combo data
+    df_layout <- introduceGNum(df_layout, e_inf, ec50, hill_coef, "_2")
+  }
+
+  if ("Gnumber_3" %in% colnames(df_layout)) { # combo data
+    df_layout <- introduceGNum(df_layout, e_inf, ec50, hill_coef, "_3")
+  }
+
+  df_layout
+}
+
+getReadoutCoef <- function(df, e_inf, ec50, hill_coef, suffix = "") {
+  apply(df, 1, function(x) {
+    clid <- x["clid"]
+    gnum <- x[paste0("Gnumber", suffix)]
+    
+    e_inf_val <- e_inf[gnum, clid]
+    ec50_val <- ec50[gnum, clid]
+    hill_val <- hill_coef[gnum, clid]
+    concentration <- as.numeric(x["Concentration"])
+    
+    e_inf_val + (1 - e_inf_val) * (ec50_val ^ hill_val / (concentration ^ hill_val + ec50_val ^ hill_val)) 
+  })
+}
+
+introduceVehicle <- function(df, suffix = "") {
+  zeroIdx <- df[, paste0("Concentration", suffix)] == 0
+  
+  df[zeroIdx, paste0("Gnumber", suffix)] <- "vehicle"
+  df[zeroIdx, paste0("DrugName", suffix)] <- "vehicle"
+  df[zeroIdx, paste0("drug_moa", suffix)] <- "vehicle"
+  
+  df
+}
+
+introduceGNum <- function(df, e_inf, ec50, hill_coef, suffix) {
+  df$ReadoutValue <- df$ReadoutValue * getReadoutCoef(df, e_inf, ec50, hill_coef, suffix)
+  df <- introduceVehicle(df, suffix)
+  
+  df
+}
+
+#' Add data with day 0
+#'
+#' @param df_merged data.frame with merged data
+#' @param noise_level numeric scalar with the level of noise added to the data
+#'
+#' @examples
+#' 
+#' cell_lines <- create_synthetic_cell_lines()
+#' drugs <- create_synthetic_drugs()
+#' data <- prepareData(cell_lines[seq_len(2), ], drugs[seq_len(4), ])
+#' data$Duration <- 72
+#' data$ReadoutValue <- 0
+#' add_day0_data(data)
+#'
+#' @return data.frame with day0 data
+#' @export
+#'
+add_day0_data <- function(df_merged, noise_level = 0.05) {
+  cond <- ifelse(
+    array("Concentration_2", nrow(df_merged)) %in% colnames(df_merged),
+    df_merged$Concentration_2 == 0, 
+    TRUE
+  )
+  df_Day0 <- unique(df_merged[df_merged$Concentration == 0 & cond, ])
+  
+  df_Day0$ReadoutValue <- df_Day0$ReadoutValue / 2 ^ (df_Day0$Duration / df_Day0$ReferenceDivisionTime)
+  coef <- (1 - noise_level / 2 + noise_level * stats::runif(nrow(df_Day0)))
+  df_Day0$ReadoutValue <- round(df_Day0$ReadoutValue * coef, 1)
+  
+  df_Day0$Duration <- 0
+  df_Day0$Barcode <- "plate_0"
+  
+  df_merged <- rbind(df_merged, df_Day0)
+  df_merged
+}
---
+++ inst/scripts/generate_example_data.R
@@ -0,0 +1,103 @@
+# generating three types of example/test data
+#
+#  The data have no day0 information.
+#  dataset with * are to be imported as example for visualization
+#
+#  "small_no_noise"           10 drugs (3 differents drug_moa) by 10 lines (3 tissues); 
+#                             single agent - no noise in the data
+#  "small"                *    10 drugs (3 differents drug_moa) by 10 lines (3 tissues); single agent
+#  "wLigand"              *    3 drugs by 4 lines (3 tissues); "Ligand = 0.1" as reference; single agent
+#  "medium"               *    40 drugs (6 differents drug_moa) by 15 lines (3 tissues); single agent
+#  "many_lines"           *    150 drugs (6 differents drug_moa) by 10 lines (3 tissues); single agent
+#  "many_drugs"           *    150 drugs (6 differents drug_moa) by 10 lines (3 tissues); single agent
+#  "combo_2dose_nonoise"  *    3 drugs x 2 co-treatment (1 drug at 2 doses) by 3 lines; 
+#                              co-treatment drug is only as DrugName_2
+#  "combo_2dose_nonoise2"      3 drugs x 2 co-treatment (1 drug at 2 doses) by 3 lines; 
+#                              co-treatment drug is also as single agent as DrugName
+#  "combo_2dose_nonoise3"      3 drugs x 2 co-treatment (1 drug at 2 doses) by 3 lines; 
+#                              co-treatment drug does NOT have single agent response
+#  "combo_1dose_many_drugs" *  149 drugs x 1 drug (1 dose) by 3 lines;
+#  "combo_matrix_small"        3 x 2 drugs (matrix) for 2 cell lines; no noise
+#  "combo_matrix"           *  6 x 3 drugs (matrix) for 8 cell lines
+#  "combo_triple"           *  2 x 3 x 2 drugs (few doses) for 4 cell lines
+#  "combo_codilution_small"    4 x 1 drugs (co-dilution) for 2 cell lines; no noise
+#  "combo_codilution"          12 x 1 drugs (co-dilution) for 8 cell lines; no noise
+
+library(SummarizedExperiment)
+library(BumpyMatrix)
+library(gDRutils)
+library(gDRcore)
+library(reshape2)
+
+set.seed(2)
+
+cell_lines <- create_synthetic_cell_lines()
+drugs <- create_synthetic_drugs()
+e_inf <- generate_e_inf(drugs, cell_lines)
+ec50 <- generate_ec50(drugs, cell_lines)
+hill_coef <- generate_hill_coef(drugs, cell_lines)
+
+#### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# generate the data for the 1st test set: no noise
+#   only for testing purpuses not displayed as example
+generateNoNoiseRawData(cell_lines, drugs, e_inf, ec50, hill_coef)
+
+#### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# generate the data for the 1st test set with noise
+generateNoiseRawData(cell_lines, drugs, e_inf, ec50, hill_coef)
+
+#### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# generate the data for the 1st test set with ligand as reference
+generateLigandData(cell_lines, drugs, e_inf, ec50, hill_coef)
+
+#### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# generate the data for the 2nd (medium size) test set with single agent
+generateMediumData(cell_lines, drugs, e_inf, ec50, hill_coef)
+
+#### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# generate the data for the 3rd (many lines) test set with single agent
+generateManyLinesData(cell_lines, drugs, e_inf, ec50, hill_coef)
+
+#### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# generate the data for the test set with single agent (many drugs)
+generateManyDrugsData(cell_lines, drugs, e_inf, ec50, hill_coef)
+
+#### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# generate the data for the test set with combo (two single dose)
+#   co-treatment drug is only as DrugName_2
+generateComboNoNoiseData(cell_lines, drugs, e_inf, ec50, hill_coef)
+
+#### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# generate the data for the test set with combo (two single dose)
+#   co-treatment drug is also as single agent as DrugName
+generateComboNoNoiseData2(cell_lines, drugs, e_inf, ec50, hill_coef)
+
+#### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# generate the data for the 3rd test set with combo (two single dose)
+#   co-treatment drug does NOT have single agent response
+generateComboNoNoiseData3(cell_lines, drugs, e_inf, ec50, hill_coef)
+
+#### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# generate the data for the test set with combo (unique dose; many drug)
+generateComboManyDrugs(cell_lines, drugs, e_inf, ec50, hill_coef)
+
+#### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# generate the data with combo matrix (small, no noise)
+generateComboMatrixSmall(cell_lines, drugs, e_inf, ec50, hill_coef)
+
+#### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# generate the data with combo matrix (mid-size)
+generateComboMatrix(cell_lines, drugs, e_inf, ec50, hill_coef)
+
+#### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# generate the data with triple combo  (no noise)
+generateTripleComboMatrix(cell_lines, drugs, e_inf, ec50, hill_coef)
+
+#### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# generate the data with combo co-dilution (small)
+generateCodilutionSmall(cell_lines, drugs, e_inf, ec50, hill_coef)
+
+#### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# generate the data for the test set with combo (co-dilution)
+generateCodilution(cell_lines, drugs, e_inf, ec50, hill_coef)
+
