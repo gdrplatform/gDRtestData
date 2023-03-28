7cdddb575a6c6764d59c8520b8188b0d582f5cf3
czechb3 <bartosz.czech@contractors.roche.com>
2023-03-28 15:10:01 +0200

    ci: update notes

--- rplatform/valid_notes2.R
+++ rplatform/valid_notes2.R
@@ -1,11 +1,11 @@
 global_valid_notes_l <- list(
   list(
-    length = 5,
+    length = 4,
     index_to_check = 2,
     text_to_check = "installed size is"
   ),
   list(
-    length = 4,
+    length = 5,
     index_to_check = 2,
     text_to_check = "Found the following hidden files and directories"
   )
