9a48724fec184493beacdad46ff29c7a708a38ea
czechb3 <bartosz.w.czech@gmail.com>
2024-01-04 14:34:30 +0100

    refactor: update duplicated functions

--- R/helper_functions.R
+++ R/helper_functions.R
@@ -1,23 +1,18 @@
 # Helper functions
-save_qs <- function(qsObj, qsName) {
-  qs::qsave(
-    qsObj,
-    file.path(system.file("testdata", package = "gDRtestData"), qsName), 
-    compress = "gzip"
-  )
-}
-
+#' @keywords internal
 prepareData <- function(cell_lines, drugs, conc = 10 ^ (seq(-3, 1, 0.5))) {
-  df_layout <- data.table::setDT(merge.data.frame(cell_lines, drugs, by = NULL))
+  df_layout <- drugs[, as.list(cell_lines), names(drugs)]
   df_layout <- add_data_replicates(df_layout)
   add_concentration(df_layout, conc)
 }
 
+#' @keywords internal
 prepareMergedData <- function(cell_lines, drugs, noise = 0.1) {
   df <- prepareData(cell_lines, drugs)
   generate_response_data(df, noise)
 }
 
+#' @keywords internal
 prepareComboMergedData <- function(cell_lines, 
                                    drugs, 
                                    drugsIdx1 = 2:4,
@@ -29,7 +24,8 @@ prepareComboMergedData <- function(cell_lines,
   
   df_2 <- cbind(drugs[drugsIdx2, ], Concentration = concentration)
   colnames(df_2) <- paste0(colnames(df_2), "_2")
-  df_layout_2 <- data.table::CJ(df_layout, df_2, by = NULL)
+  
+  df_layout_2 <- data.table::as.data.table(merge.data.frame(df_layout, df_2, by = NULL))
   if (modifyDf2) {
     df_layout_2 <- df_layout_2[!(df_layout_2$Concentration == 0 & df_layout_2$Concentration_2 > 0), ]
   }
@@ -37,20 +33,13 @@ prepareComboMergedData <- function(cell_lines,
   generate_response_data(df_layout_2, noise)
 }
 
+#' @keywords internal
 prepareCodilutionData <- function(df, df_layout) {
   colnames(df) <- paste0(colnames(df), "_2")
   df_2 <- cbind(df_layout, df)
   df_2 <- df_2[df_2$DrugName != df_2$DrugName_2, ]
-  rows <- df_2$Concentration_2 > 0
+  rows <- which(df_2$Concentration_2 > 0)
   cols <- c("Concentration", "Concentration_2")
-  df_2[rows, cols] <- df_2[rows, cols] / 2
-  
+  df_2[rows, (cols) := lapply(.SD, function(x) x / 2), .SDcols = cols]
   df_2
-}
-
-changeColNames <- function(df, drugs, suffix) {
-  cols <- colnames(df) %in% c(colnames(drugs), "Concentration")
-  colnames(df)[cols] <- paste0(colnames(df)[cols], suffix)
-  
-  df
 }
