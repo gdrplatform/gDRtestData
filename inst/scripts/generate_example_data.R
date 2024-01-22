3bac51d4c567cb26a219d038bec4e1afb65ebd6a
Janina Smola <janina.smola@contractors.roche.com>
2024-01-22 15:39:17 +0100

    chore: update dataset script

--- inst/scripts/generate_example_data.R
+++ inst/scripts/generate_example_data.R
@@ -3,25 +3,25 @@
 #  The data have no day0 information.
 #  dataset with * are to be imported as example for visualization
 #
-#  "small_no_noise"            10 drugs (3 different drug_moa) by 10 lines (3 tissues); 
-#                              single agent - no noise in the data
-#  "small"                  *  10 drugs (3 different drug_moa) by 10 lines (3 tissues); single agent
-#  "wLigand"                *  3 drugs by 4 lines (3 tissues); "Ligand = 0.1" as reference; single agent
-#  "medium"                 *  40 drugs (6 different drug_moa) by 15 lines (3 tissues); single agent
-#  "many_lines"             *  150 drugs (6 different drug_moa) by 10 lines (3 tissues); single agent
-#  "many_drugs"             *  150 drugs (6 different drug_moa) by 10 lines (3 tissues); single agent
-#  "combo_2dose_nonoise"    *  3 drugs x 2 co-treatment (1 drug at 2 doses) by 3 cell lines; 
-#                              co-treatment drug occurs also as a primary drug
-#  "combo_2dose_nonoise2"      3 drugs x 2 co-treatment (1 drug at 2 doses) by 3 cell lines; 
-#                              co-treatment drug is also as single agent as DrugName
-#  "combo_2dose_nonoise3"      3 drugs x 2 co-treatment (1 drug at 2 doses) by 3 cell lines; 
-#                              co-treatment drug does NOT have single agent response
-#  "combo_1dose_many_drugs" *  149 drugs x 1 drug (1 dose) by 3 lines;
-#  "combo_matrix_small"        3 x 2 drugs (matrix) for 2 cell lines; no noise
-#  "combo_matrix"           *  6 x 3 drugs (matrix) for 8 cell lines
-#  "combo_triple"           *  2 x 3 x 2 drugs (triple matrix) for 4 cell lines
-#  "combo_codilution_small"    4 x 1 drugs (co-dilution) for 2 cell lines; no noise
-#  "combo_codilution"          12 x 1 drugs (co-dilution) for 8 cell lines; no noise
+#  "small_no_noise"             10 drugs (3 different drug_moa) by 10 lines (3 tissues); 
+#                               single agent - no noise in the data
+#  "small"                  *   10 drugs (3 different drug_moa) by 10 lines (3 tissues); single agent
+#  "wLigand"                *   3 drugs by 4 lines (3 tissues); "Ligand = 0.1" as reference; single agent
+#  "medium"                 *   40 drugs (6 different drug_moa) by 15 lines (3 tissues); single agent
+#  "many_lines"             *   150 drugs (6 different drug_moa) by 10 lines (3 tissues); single agent
+#  "many_drugs"             *   150 drugs (6 different drug_moa) by 10 lines (3 tissues); single agent
+#  "combo_2dose_nonoise"    *   3 drugs x 2 co-treatment (1 drug at 2 doses) by 3 cell lines; 
+#                               co-treatment drug occurs also as a primary drug
+#  "combo_2dose_nonoise2"       3 drugs x 2 co-treatment (1 drug at 2 doses) by 3 cell lines; 
+#                               co-treatment drug occurs also as a primary drug
+#  "combo_2dose_nonoise3"       3 drugs x 2 co-treatment (1 drug at 2 doses) by 3 cell lines; 
+#                               co-treatment drug does NOT have single agent response
+#  "combo_1dose_many_drugs" *   149 drugs x 1 drug (1 dose) by 3 lines;
+#  "combo_matrix_small"         3 x 2 drugs (matrix) for 2 cell lines; no noise
+#  "combo_matrix"           *   6 x 3 drugs (matrix) for 8 cell lines
+#  "combo_triple"           *   2 x 3 x 2 drugs (triple matrix) for 4 cell lines
+#  "combo_codilution_small"     4 x 1 drugs (co-dilution) for 2 cell lines; no noise
+#  "combo_codilution"           12 x 1 drugs (co-dilution) for 8 cell lines; no noise
 
 library(SummarizedExperiment)
 library(BumpyMatrix)
